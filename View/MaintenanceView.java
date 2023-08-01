d3bdf5912cd226bac19cf3650f60327c4548dfdb
amieltenerife <amiel_elijah_tenerife@dlsu.edu.ph>
2023-08-01 22:30:11 +0800

    maintenance GUI (so far)

---
+++ View/MaintenanceView.java
@@ -0,0 +1,44 @@
+package View;
+
+import Controller.VendingMachineController;
+
+import javax.swing.*;
+
+public class MaintenanceView extends JFrame{
+    private VendingMachineController controller;
+    private JLabel vendingMachineName;
+    private JButton backButton;
+    private JTabbedPane sectionPanels;
+    private JPanel item1;
+    private JPanel item2;
+    private JPanel item3;
+    private JPanel item4;
+    private JPanel item5;
+    private JPanel item6;
+    private JPanel item7;
+    private JPanel item8;
+    private JPanel item9;
+    private JButton item1Plus;
+    private JButton item2Plus;
+    private JButton item3Plus;
+    private JButton item4Plus;
+    private JButton item5Plus;
+    private JButton item6Plus;
+    private JButton item7Plus;
+    private JButton item8Plus;
+    private JButton item9Plus;
+    private JLabel item1Name;
+    private JLabel item2Name;
+    private JLabel item3Name;
+    private JLabel item4Name;
+    private JLabel item5Name;
+    private JLabel item6Name;
+    private JLabel item7Name;
+    private JLabel item8Name;
+    private JLabel item9Name;
+    private JPanel maintenanceMenu;
+    private JButton replenishAllButton;
+    private JPanel transactionSummary;
+    private JPanel displaySummary;
+    private JButton collectRevenueButton;
+}
